c5d88a70815fda7f47a6586bcfab2c39e7d22ef4
goodleaf96 <ky1234tho@gmail.com>
2025-02-26 11:47:32 +0900

    백엔드와 연결을 위한 config 파일 변경

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MainPage from '../pages/MainPage.vue'
-import DetailPage from '../pages/DetailPage.vue'
+import MainPage from '@/pages/MainPage.vue'
+import DetailPage from '@/pages/DetailPage.vue'
 
 const routes = [
     {
